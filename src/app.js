c074a00f7554575519f191ee0aae02d03370cc09
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-02-08 00:46:48 +0530

    first commit -render buttons finished

--- src/app.js
+++ src/app.js
@@ -46,26 +46,162 @@ function renderPatty() {
 }
 
 function renderCheese() {
-  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese");
+  //you can also use getElementById
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
-  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomatoes = document.querySelector("#tomato");
+  //you can also use getElementById
+  if (state.Tomatoes) {
+    tomatoes.style.display = "inherit";
+  } else {
+    tomatoes.style.display = "none";
+  }
 }
 
 function renderOnions() {
-  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onion = document.querySelector("#onion");
+  //you can also use getElementById
+  if (state.Onions) {
+    onion.style.display = "inherit";
+  } else {
+    onion.style.display = "none";
+  }
 }
 
 function renderLettuce() {
-  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce");
+  //you can also use getElementById
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+  }
 }
+
+function renderButtons(){
+  let patty = document.querySelector(".btn-patty");
+  let tomato = document.querySelector(".btn-tomatoes");
+  let onion = document.querySelector(".btn-onions");
+  let lettuce =document.querySelector(".btn-lettuce");
+  let cheese = document.querySelector(".btn-cheese");
+  
+  if(!state.Patty)
+  {
+   var cl = "button" + " btn-patty";
+   patty.className = cl;
+  }
+  else{
+    var cl = patty.classList;
+   patty.className = cl + " active";
+  }
+
+  if(!state.Tomatoes)
+  {
+   var cl = "button" + " btn-tomatoes";
+   tomato.className = cl;
+  }
+  else{
+    var cl = tomato.classList;
+   tomato.className = cl + " active";
+  }
+
+  if(!state.Lettuce)
+  {
+   var cl = "button" + " btn-lettuce";
+   lettuce.className = cl;
+  }
+  else{
+    var cl = lettuce.classList;
+   lettuce.className = cl + " active";
+  }
+
+  if(!state.Onions)
+  {
+   var cl = "button" + " btn-onions";
+   onion.className = cl;
+  }
+  else{
+    var cl = onion.classList;
+   onion.className = cl + " active";
+  }
+
+  if(!state.Cheese)
+  {
+   var cl = "button" + " btn-cheese";
+   cheese.className = cl;
+  }
+  else{
+    var cl = cheese.classList;
+   cheese.className = cl + " active";
+  }
+}
+
+function  renderIngredientsBoard(){
+
+
+
+}
+
+function renderPrice()
+{
+  let price = document.querySelector('.price-details');
+  let inr = "INR ";
+  if(state.Patty)
+  {
+    ingredients.Patty = 80;
+  }
+  else{
+    ingredients.Patty = 0;
+  }
+  if(state.Tomatoes)
+  {
+    ingredients.Tomatoes = 10;
+  }
+  else{ingredients.Tomatoes = 0};
+  if(state.Cheese){ingredients.Cheese = 20}
+  else{ingredients.Cheese =0}
+  if(state.Lettuce){ingredients.Lettuce = 20}
+  else{ingredients.Lettuce =0};
+  if(state.Onions){ingredients.Onions = 20}
+  else{ingredients.Onions = 0}
+  let totalPrice = ingredients.Patty + ingredients.Lettuce + ingredients.Cheese + ingredients.Onions + ingredients.Tomatoes + wholeWheatBun*2;
+  price.innerHTML =inr + totalPrice;
+}
+
+
+
 
 document.querySelector(".btn-patty").onclick = function () {
   state.Patty = !state.Patty;
   renderAll();
 };
 
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
+
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
+
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
+
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 // Trial 2 - Setup event listener for the cheese button
 
 
